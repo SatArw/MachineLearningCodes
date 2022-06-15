54897ff407be95ac5aceab359fd9374fdbd11602
Satwik Arawalli <96329337+SatArw@users.noreply.github.com>
2022-06-15 22:06:29 +0530

    Add files via upload

---
+++ ex6/dataset3Params.m
@@ -0,0 +1,70 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+all_C = [0.01, 0.03, 0.1, 0.3 , 1, 3, 10, 30];
+all_sigma = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+
+errors = zeros(size(all_C,2),size(all_sigma,2));
+
+for i = 1:size(all_C,2)
+    for j = 1:size(all_sigma,2)
+        curr_model = svmTrain(X,y,all_C(1,i),@(x1, x2)gaussianKernel(x1, x2, all_sigma(1,j)));
+        curr_preds = svmPredict(curr_model,Xval);
+        errors(i,j) = mean(double(curr_preds ~= yval));
+    end
+end
+
+min_error = errors(1,1);
+min_indices = [1,1];
+for i = 1:size(all_C,2)
+    for j = 1:size(all_sigma,2)
+        if(errors(i,j)<min_error)
+            min_error = errors(i,j)
+            min_indices = [i,j];    
+        end
+    end
+end
+
+C = all_C(min_indices(1))
+sigma = all_sigma(min_indices(2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% =========================================================================
+
+end
